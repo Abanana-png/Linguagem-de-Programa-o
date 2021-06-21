47d3bbef747f05850e161193daac5931b7f14ac4
Abanana-png <83030475+Abanana-png@users.noreply.github.com>
2021-06-21 20:55:32 -0300

    Add files via upload

--- main.cs
+++ main.cs
@@ -3,59 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-class MainClass {
-  public static void Main (string[] args) 
+class Program
+{
+  static void Main(string[] args)
   {
-    Loja Cesta = new Loja();
-   
-    Personagem Cliente = new Personagem(nome:"Cliente");
+    Interador listinha = new Interador();
 
-
-
-
-
-    Item espada = new Item(nome:"Espada", descricao:"Afiada",moeda: 90);
-    Cesta.Catalogo.Add(espada);
-
-    Item borracha = new Item(nome:"Borracha", descricao:"Afiada",moeda: 10);
-    Cesta.Catalogo.Add(borracha);
-
-    Item pizza = new Item(nome:"Pizza", descricao:"Afiada",moeda: 2);
-    Cesta.Catalogo.Add(pizza);
-
-    Item cachorro = new Item(nome:"Cachorro", descricao:"Afiada",moeda: 90);
-    Cesta.Catalogo.Add(cachorro);
-
-
-
-    Cesta.ImpCat();
-    string Escolha;
-
-    do{
-      
-
-
-    
-
-
-
-     
-
-     Cliente.ComprarItem(Cliente.Comprar(), Cesta.Catalogo);
-     Cliente.ImprimirPersonagem();
-     Console.WriteLine("Digite 1 para CONTINUAR ou 444 para FECHAR");
-     Escolha = Console.ReadLine();
-     Cesta.ImpCat();
-    }
-    while(Escolha == "1");
-    if(Escolha != "1")
-    {
-      Console.WriteLine("\r\nOkay, " + Cliente.Nome + "!" + " Feche a porta ao sair e volte sempre!");
-    }
-    
-
-
-     
-      
+    bool showMenu = true;
+      while (showMenu)
+      {
+        showMenu = MainMenu(listinha);
+      }
   }
+  private static bool MainMenu(Interador listinha)
+  {          
+    Console.WriteLine("\r\nEscolha sua opção:");
+    Console.WriteLine("1) Retângulo");
+    Console.WriteLine("2) Círculo");
+    Console.WriteLine("3) Mostrar anteriores");
+    Console.WriteLine("4) Mostrar anteriores e sair");
+    Console.Write("\r\nDigite sua opção: ");
+ 
+    switch (Console.ReadLine())
+    {
+      case "1":
+      Console.Clear();
+      Console.Write("\r\nEntre a base e depois entre com altura do retângulo.\n");
+      Retangulo ret = new Retangulo(largura: Convert.ToDouble(Console.ReadLine()), altura: Convert.ToDouble(Console.ReadLine()));
+      listinha.Figuras.Add(ret);
+
+      // acessando métodos get
+      Console.WriteLine("\r\nBase:\t\t{0:0.0}", ret.Largura);
+      Console.WriteLine("Altura:\t\t{0:0.0}", ret.Altura);
+      ret.ImprimirFig();
+
+      return true;
+      
+      
+      case "2":
+      Console.Clear();
+      Console.Write("\r\nEntre com o raio do círculo.\n");
+      Circulo cir = new Circulo(raio: Convert.ToDouble(Console.ReadLine()));
+      listinha.Figuras.Add(cir);
+
+      // acessando métodos get
+      Console.WriteLine("\r\nRaio:\t\t{0:0.0}", cir.Raio);
+      cir.ImprimirFig();
+
+      return true;
+
+      case "3":
+      Console.Clear();
+      listinha.ImprimirFiguras();
+      return true;
+
+      case "4":
+      Console.Clear();
+      if (listinha.Figuras.Count == 0)
+      {
+          Console.WriteLine("Sem itens");
+
+          Impression();
+      }
+      else
+      {
+        listinha.ImprimirFiguras();
+
+        Impression();
+      }
+      return false;
+      default:
+      return true;
+    }
+  }
+
+  
 }
